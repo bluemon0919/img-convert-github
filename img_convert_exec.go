5b9f2201be5ec1ed70ade1aa35a7bb8383aa0361
bluemon0919 <bluemon0919@yahoo.co.jp>
2018-06-11 17:43:14 +0900

    レビュー修正

--- img_convert_exec.go
+++ img_convert_exec.go
@@ -3,82 +3,70 @@ package main
 import (
 	"flag"
 	"fmt"
-	"img-convert-github/imgconv"
 	"io/ioutil"
-	"os"
-	"path"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+
+	"img-convert-github/imgconv"
 )
 
-var targetDir = flag.String("dir", "", "フォーマット変換を行うディレクトリを指定する。")
-var in = flag.String("in", "jpg", "入力画像フォーマットを指定する。対応フォーマット:jpg, png")
-var out = flag.String("out", "png", "出力画像フォーマットを指定する。対応フォーマット:jpg, png")
-var jquality *int = flag.Int("jquality", 100, "変換クオリティを指定する。（0-100）")
+var (
+	targetDir = flag.String("dir", "", "フォーマット変換を行うディレクトリを指定する。")
+	in        = flag.String("in", "jpg", "入力画像フォーマットを指定する。対応フォーマット:jpg, png")
+	out       = flag.String("out", "png", "出力画像フォーマットを指定する。対応フォーマット:jpg, png")
+	jquality  = flag.Int("jquality", 100, "変換クオリティを指定する。（0-100）")
+)
 
 func main() {
-	var ic imgconv.ImgConvert
 	var err error
 	//convertToPngFromDirectry(`C:\Users\bluem\go\img`)
+	td := `C:\Users\bluem\go\img`
 	flag.Parse()
+	targetDir = &td
 
 	var dirList []string
-	dirList = getFileName(*targetDir)
-	for _, path := range dirList {
-		if ic, err = setting(path); err == nil {
+	dirList, err = getFileName(*targetDir)
+	if err == nil {
+		for _, path := range dirList {
+			ic := imgconv.NewImgConvert(*in, *out, path, *jquality)
+			if ic == nil {
+				break
+			}
 			err = ic.ConvertTo()
+			if err != nil {
+				break
+			}
 		}
 	}
 
 	if err != nil {
 		fmt.Println("[ERROR]", err)
+		fmt.Println("Error occurrece")
 	}
-}
-
-// setting receives and sets parameters necessary for image conversion from the command line.
-func setting(path string) (imgconv.ImgConvert, error) {
-	var ic imgconv.ImgConvert
-	ic.InFormat = imgconv.NON
-	ic.OutFormat = imgconv.NON
-	ic.Path = path
-	ic.Jquality = *jquality
-	if "jpg" == *in {
-		ic.InFormat = imgconv.JPG
-	} else if "png" == *in {
-		ic.InFormat = imgconv.PNG
-	} else {
-		return ic, fmt.Errorf("The parameter `in` is incorrect.")
-	}
-	if "jpg" == *out {
-		ic.OutFormat = imgconv.JPG
-	} else if "png" == *out {
-		ic.OutFormat = imgconv.PNG
-	} else {
-		return ic, fmt.Errorf("The parameter `out` is incorrect.")
-	}
-	if ic.InFormat == ic.OutFormat {
-		return ic, fmt.Errorf("Cannot convert format.")
-	}
-	return ic, nil
 }
 
 // getFileName get the list of file names under the specified directory.
-func getFileName(dir string) []string {
+func getFileName(dir string) ([]string, error) {
 	var paths []string
-	var files []os.FileInfo
-	var err error
-	if files, err = ioutil.ReadDir(dir); err != nil {
-		println("Error", err)
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, errors.Wrap(err, "ReadDir faild")
 	}
 	for _, file := range files {
 		if file.IsDir() {
-			var underPaths []string
-			underPaths = getFileName(dir + `\` + file.Name())
+			underPaths, err := getFileName(filepath.Join(dir, file.Name()))
+			if err != nil {
+				return nil, errors.Wrap(err, "getFileName faild")
+			}
 			for _, path := range underPaths {
 				paths = append(paths, path)
 			}
-		} else if path.Ext(file.Name()) == ".jpg" {
-			paths = append(paths, dir+`\`+file.Name())
-		} else {
+		} else if ext := filepath.Ext(file.Name()); ext != "" {
+			if (*in == "jpg" && ext == ".jpg") || (*in == "png" && ext == ".png") {
+				paths = append(paths, filepath.Join(dir, file.Name()))
+			}
 		}
 	}
-	return paths
+	return paths, nil
 }
--- imgconv/my_convert.go
+++ imgconv/my_convert.go
@@ -4,90 +4,130 @@
 package imgconv
 
 import (
-	"errors"
-	"fmt"
 	"image"
-	_ "image/jpeg"
-	_ "image/png"
+	"image/jpeg"
+	"image/png"
 	"os"
 	"path"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
-// Format is defines the image format.
-type Format int
+type ImgConvert interface {
+	ConvertTo() error
+	decodeTo() (image.Image, error)
+	encodeTo(img image.Image) error
+}
 
 // ImgConvert is a parameter required for image format ImgConvert.
 // Set input / output format, input image path, etc.
-type ImgConvert struct {
+type ImgConvInfo struct {
 	InFormat  string // InFormat is input image format.
-	OutFormat Format // OutFormat is output image format.
+	OutFormat string // OutFormat is output image format.
 	Path      string // TargetDir is input image path
-	Jquality  int    // Jquality is the quality when converting JPEG.
+	extPath   string
+	//	Jquality  int    // Jquality is the quality when converting JPEG.
 }
 
-const (
-	// NON is the initial value and is prohibited.
-	NON Format = 0
-	//JPG is JPEG format.
-	JPG Format = 1
-	// PNG is PNG format.
-	PNG Format = 2
-)
+type ImgJpgConvert struct {
+	*ImgConvInfo
+	Jquality int // Jquality is the quality when converting JPEG.
+}
+
+type ImgPngConvert struct {
+	*ImgConvInfo
+}
+
+// NewImgConvert generates an image change object from the input parameters.
+func NewImgConvert(inFormat string, outFormat string, path string, jquality int) ImgConvert {
+	p := &ImgConvInfo{inFormat, outFormat, path, ""}
+	if "jpg" == outFormat {
+		return &ImgJpgConvert{p, jquality}
+	}
+	if "png" == outFormat {
+		return &ImgPngConvert{p}
+	}
+	return nil
+}
 
 // ConvertTo converts the image format according to the given parameters.
-func (ic *ImgConvert) ConvertTo() error {
-	var err error
-	var img image.Image
-	if img, err = ic.decodeTo(); err != nil {
+func (ic *ImgConvInfo) ConvertTo() error {
+	return errors.New("there is no implementation in the specified type")
+}
+
+// ConvertTo converts the image format according to the given parameters.
+func (ic *ImgJpgConvert) ConvertTo() error {
+	img, err := ic.decodeTo()
+	if err != nil {
 		return err
 	}
-	if err = ic.encodeTo(img); err != nil {
+	ic.extPath = ic.Path[:strings.LastIndex(ic.Path, path.Ext(ic.Path))]
+	err = ic.encodeTo(img)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ConvertTo converts the image format according to the given parameters.
+func (ic *ImgPngConvert) ConvertTo() error {
+	img, err := ic.decodeTo()
+	if err != nil {
+		return err
+	}
+	ic.extPath = ic.Path[:strings.LastIndex(ic.Path, path.Ext(ic.Path))]
+	err = ic.encodeTo(img)
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
 // decodeTo decodes image data to image.Image format.
-func (ic *ImgConvert) decodeTo() (image.Image, error) {
+func (ic *ImgConvInfo) decodeTo() (image.Image, error) {
 	var img image.Image
 	inputFile, err := os.Open(ic.Path)
 	defer inputFile.Close()
 	if nil != err {
 		return img, errors.Wrap(err, "file open error")
 	}
-	if img, ic.InFormat, err = image.Decode(inputFile); err != nil {
+
+	img, ic.InFormat, err = image.Decode(inputFile)
+	if err != nil {
 		return img, errors.Wrap(err, "decode error")
 	}
 	return img, nil
 }
 
 // encodeTo generates an image file according to specified parameters.
-func (ic *ImgConvert) encodeTo(img image.Image) error {
-	var outputFile *os.File
-	var err error
-	withoutExt := ic.Path[:strings.LastIndex(ic.Path, path.Ext(ic.Path))]
-	if JPG == ic.OutFormat {
-		if outputFile, err = os.Create(withoutExt + ".jpg"); err != nil {
-			return errors.Wrap(err, "create file error")
-		}
-		println("path" + path.Ext(ic.Path))
-		defer outputFile.Close()
+func (ic *ImgConvInfo) encodeTo(img image.Image) error {
+	return errors.New("there is no implementation in the specified type")
+}
 
-		opts := &jpeg.Options{Quality: ic.Jquality}
-		if err := jpeg.Encode(outputFile, img, opts); err != nil {
-			return fmt.Errorf("jpeg.encode error")
-		}
-	} else {
-		if outputFile, err = os.Create(withoutExt + ".png"); err != nil {
-			return fmt.Errorf("create error")
-		}
-		println("path" + path.Ext(ic.Path))
-		defer outputFile.Close()
+// encodeTo generates an image file according to specified parameters.
+func (ic *ImgJpgConvert) encodeTo(img image.Image) error {
+	outputFile, err := os.Create(ic.extPath + ".jpg")
+	defer outputFile.Close()
+	if err != nil {
+		return errors.Wrap(err, "create error")
+	}
+	opts := &jpeg.Options{Quality: ic.Jquality}
+	if err := jpeg.Encode(outputFile, img, opts); err != nil {
+		return errors.Wrap(err, "encode error")
+	}
+	return nil
+}
 
-		if err := png.Encode(outputFile, img); err != nil {
-			return fmt.Errorf("png.encode error")
-		}
+// encodeTo generates an image file according to specified parameters.
+func (ic *ImgPngConvert) encodeTo(img image.Image) error {
+	outputFile, err := os.Create(ic.extPath + ".png")
+	defer outputFile.Close()
+	if err != nil {
+		return errors.Wrap(err, "create error")
+	}
+	if err := png.Encode(outputFile, img); err != nil {
+		return errors.Wrap(err, "encode error")
 	}
 	return nil
 }
